cc8035b8c9204fdf81f471007dcef3188913f918
Emerson Junqueira <emoonlit@gmail.com>
2024-05-11 20:04:20 -0300

    refactor: removed not implemented notification code

--- background.js
+++ background.js
@@ -26,32 +26,6 @@ function codeToInject() {
   });
 }
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      function: codeToInject
-    });
-  }
-});
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: copyNodeId
-  }, (result) => {
-    console.log(result);
-
-    if (result.length && result[0].result) {
-      createToastNotification('success', 'Node ID ' + result[0].result + ' Success!');
-    } else if (chrome.runtime.lastError) {
-      createToastNotification('error', chrome.runtime.lastError.message);
-    } else {
-      createToastNotification('error', 'Node ID not found.');
-    }
-  });
-});
-
 function copyNodeId() {
   const url = window.location.href;
   const parts = url.split('node=');
@@ -75,24 +49,18 @@ function copyNodeId() {
   }
 }
 
-// TODO: Implement notification
-function createToastNotification(type, message) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: codeToInject
+    })
+  }}
+);
 
-  const toast = document.createElement('div');
-  toast.style.position = 'fixed';
-  toast.style.bottom = '20px';
-  toast.style.right = '20px';
-  toast.style.padding = '10px';
-  toast.style.backgroundColor = '#444';
-  toast.style.color = '#fff';
-  toast.style.opacity = '0.9';
-  toast.style.borderRadius = '5px';
-  toast.textContent = `${type}: ${message}`;
-  toast.style.zIndex = '999';
-
-  document.body.appendChild(toast);
-
-  setTimeout(function () {
-    document.body.removeChild(toast);
-  }, 3000);
-}
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: copyNodeId
+  })
+});
